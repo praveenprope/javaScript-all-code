baa6b6d6c12fa8057a0208bc85b707dffef1fba1
praveenprope <167769624+praveenprope@users.noreply.github.com>
2024-08-24 19:25:10 +0530

    Add files via upload

---
+++ 2. Data type an Ecma Standards/script.js
@@ -0,0 +1,38 @@
+/*
+Data types are two types 
+1. Primitive Data Type
+2. Non- Primitive Data Type
+*/
+
+// Primitive Data Type 
+
+// i. String Data Type
+
+  let a = "Rakesh Chacha";
+  console.log(a,typeof a);
+
+// ii. Number Data Type
+
+  let aNumber = 7363;
+  console.log(aNumber, typeof aNumber);
+
+
+// iii. Boolean Data Type
+
+  let aBoolean = true;  // true/false
+  console.log(aBoolean, typeof aBoolean);
+
+// iv. Undefind Data Type 
+
+    let aUndefind = undefined;
+    console.log(aUndefind, typeof aUndefind);
+
+ // v. Null Data Type
+
+    let aNull = null;
+    console.log(aNull, typeof aNull);
+
+ // vi.  symbol data type
+
+
+//  2. Non-Primitive Data Type
